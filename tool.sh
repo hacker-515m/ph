233340b80e00382d4d430d2545b1527f3cb186a7
hacker-515m <bahaa0106400@gmail.com>
2024-07-15 12:33:43 +0300

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+clear
+echo -e "\033[31m
+           ▄▄         ▄▄         ▄▄         ▄▄                     
+          ███         ██        ███         ██                     
+           ██                    ██                                
+▀████████▄ ███████▄ ▀███  ▄██▀██████████▄ ▀███ ▀████████▄  ▄█▀█████
+  ██   ▀██ ██    ██   ██  ██   ▀▀██    ██   ██   ██    ██ ▄██  ██  
+  ██    ██ ██    ██   ██  ▀█████▄██    ██   ██   ██    ██ ▀█████▀  
+  ██   ▄██ ██    ██   ██  █▄   ████    ██   ██   ██    ██ ██       
+  ██████▀ ████  ████▄████▄██████▀███  ████▄████▄████  ████▄███████ 
+  ██                                                      █▀     ██
+▄████▄                                                    ██████▀  
+
+\033[0m"
+ echo "Cerated Py Bahaa Al Maghribii"
+ echo "_____________________________"
+ echo "my phone number +201064004818 "
+ echo "______________________________"
+echo "My city Sohag , Egption "
+echo "*******************************"
+echo "*******************************"
+sleep 10
+
+apt install python3
+pip install colorama
+git clone https://github.com/Cyber-Dioxide/CyberPhish
+cd CyberPhish
+pip install -r requirements.txt
+python3 CyberPhish.py
